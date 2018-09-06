029fd17a80ff82371590ccf33c0cbdef15e4efb3
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2018-09-06 17:56:43 +0200

    more css and linking profiles in nav

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Company = require('../models/Company');
-const ensureLogin = require('connect-ensure-login');
+const Company = require("../models/Company");
+const ensureLogin = require("connect-ensure-login");
 // const fs = require("fs");
-const User = require('../models/User');
+const User = require("../models/User");
 /* GET home page */
-const util = require('util');
+const util = require("util");
 
-router.get('/', (req, res, next) => {
-	res.render('index');
+router.get("/", (req, res, next) => {
+    res.render("index");
 });
 
 router.get("/user_profile", (req, res, next) => {
@@ -17,6 +17,15 @@ router.get("/user_profile", (req, res, next) => {
 
 router.get("/company_profile", (req, res, next) => {
     res.render("company_profile", { user: req.user });
+});
+
+router.get("/profile", (req, res, next) => {
+    if (req.user.collection.collectionName === "users") {
+        res.render("user_profile", { user: req.user });
+    } else if (req.user.collection.collectionName === "companies") {
+        console.log("hi from company");
+        res.render("company_profile", { user: req.user });
+    }
 });
 
 module.exports = router;
